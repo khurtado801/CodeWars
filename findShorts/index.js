ef4c5917e1326e347bfe741f93be70957e7b48e7
ken.hurtado <khurtado@protonmail.com>
2018-05-15 16:43:53 -0600

    Solved findShorts.

--- findShorts/index.js
+++ findShorts/index.js
@@ -1,5 +1,13 @@
 findShorts = (s) => {
+    let strSplit = s.split(' ');
+    let shortestStr = strSplit[0].length;
 
+    for(let i = 1; i < strSplit.length; i++) {
+        if(strSplit[i].length < shortestStr) {
+            shortestStr = strSplit[i].length;
+        }
+    }
+    return shortestStr;
 }
 
-console.log(findShorts(''));
+console.log(findShorts('bitcoin take over the world maybe who knows perhaps'));
